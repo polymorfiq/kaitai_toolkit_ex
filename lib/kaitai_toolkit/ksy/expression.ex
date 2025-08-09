5398462cf3a8533f2a7c92c973af89cdfc48be5b
Cory Finger <cory.j.finger@gmail.com>
2025-08-09 12:25:47 -0700

    Create initial lexer implementation

--- lib/kaitai_toolkit/ksy/expression.ex
+++ lib/kaitai_toolkit/ksy/expression.ex
@@ -1,25 +1,307 @@
 defmodule KaitaiToolkit.Ksy.Expression do
   @spec lex(binary(), binary(), [binary()]) :: [binary()]
   def lex(str, word \\ <<>>, lexed \\ [])
-  def lex(<<>>, <<>>, lexed), do: lexed
-  def lex(<<>>, word, lexed), do: lex(<<>>, <<>>, lexed ++ [word])
+  def lex(<<>>, <<>>, lexed), do: lex_second_stage(lexed)
+  def lex(<<>>, word, lexed), do: lex(<<>>, <<>>, lexed ++ [normalize_word(word)])
 
   def lex(<<char::binary-size(1), expr_str::binary>>, word, lexed) do
     cond do
       Regex.match?(~r|^[\s]+$|, char) && String.length(word) > 0 ->
         # Hit Whitespace - we have finished current word
-        lex(expr_str, <<>>, lexed ++ [word])
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word)])
 
       Regex.match?(~r|^[\s]+$|, char) && String.length(word) == 0 ->
         # Consecutive Whitespace - ignore it
         lex(expr_str, word, lexed)
 
-      Regex.match?(~r|^[\_]+$|, char) && Regex.match?(~r|^[0-9\_\.]+$|, word) ->
+      char == "[" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :open_square_bracket])
+
+      char == "[" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:open_square_bracket])
+
+      char == "]" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :close_square_bracket])
+
+      char == "]" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:close_square_bracket])
+
+      char == "(" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :open_parens])
+
+      char == "(" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:open_parens])
+
+      char == ")" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :close_parens])
+
+      char == ")" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:close_parens])
+
+      char == "+" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :plus])
+
+      char == "+" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:plus])
+
+      char == "-" && Regex.match?(~r|^[\-0-9\_\.e]+$|, word) ->
+        # '-' in the middle of a scientific notation. It is part of the number
+        lex(expr_str, word <> "-", lexed)
+
+      char == "-" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :minus])
+
+      char == "-" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:minus])
+
+      char == "*" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :star])
+
+      char == "*" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:star])
+
+      char == "/" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :slash])
+
+      char == "/" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:slash])
+
+      char == "%" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :percent])
+
+      char == "%" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:percent])
+
+      char == "!" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :exclamation])
+
+      char == "!" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:exclamation])
+
+      char == "&" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :ampersand])
+
+      char == "&" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:ampersand])
+
+      char == "?" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :question_mark])
+
+      char == "?" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:question_mark])
+
+      char == ":" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :colon])
+
+      char == ":" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:colon])
+
+      char == "|" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :pipe])
+
+      char == "|" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:pipe])
+
+      char == "^" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :caret])
+
+      char == "^" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:caret])
+
+      char == "<" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :left_arrow])
+
+      char == "<" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:left_arrow])
+
+      char == ">" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :right_arrow])
+
+      char == ">" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:right_arrow])
+
+      char == "=" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :equals])
+
+      char == "=" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:equals])
+
+      char == "," && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :comma])
+
+      char == "," && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:comma])
+
+      char == "\\" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :backslash])
+
+      char == "\\" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:backslash])
+
+      char == "'" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :single_quote])
+
+      char == "'" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:single_quote])
+
+      char == "\"" && String.length(word) > 0 ->
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :double_quote])
+
+      char == "\"" && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:double_quote])
+
+      char == "_" && Regex.match?(~r|^[\-0-9\_\.]+$|, word) ->
         # '_' in the middle of a number. Easier to read. Ignore it
         lex(expr_str, word, lexed)
+
+      char == "_" && Regex.match?(~r|^0b[01]+$|, word) ->
+        # '_' in the middle of a binary number. Easier to read. Ignore it
+        lex(expr_str, word, lexed)
+
+      char == "_" && Regex.match?(~r|^0o[01]+$|, word) ->
+        # '_' in the middle of an octal number. Easier to read. Ignore it
+        lex(expr_str, word, lexed)
+
+      char == "_" && Regex.match?(~r|^0x[01]+$|, word) ->
+        # '_' in the middle of a hex number. Easier to read. Ignore it
+        lex(expr_str, word, lexed)
+
+      char == "." && Regex.match?(~r|^[\-0-9\_\.]+$|, word) ->
+        # '.' in the middle of a number. It is part of the number.
+        lex(expr_str, word <> ".", lexed)
+
+      char == "." && String.length(word) > 0 ->
+        # '.' not in the middle of a number - it is special
+        lex(expr_str, <<>>, lexed ++ [normalize_word(word), :dot])
+
+      char == "." && String.length(word) == 0 ->
+        lex(expr_str, word, lexed ++ [:dot])
 
       true ->
         lex(expr_str, word <> char, lexed)
     end
   end
+
+  defp normalize_word("true"), do: :true
+  defp normalize_word("false"), do: :false
+  defp normalize_word(word) do
+    cond do
+      word == "not" ->
+        :not
+
+      word == "and" ->
+        :and
+
+      word == "or" ->
+        :or
+
+      word == "_io" ->
+        :io
+
+      word == "_root" ->
+        :root
+
+      word == "_parent" ->
+        :parent
+
+      Regex.match?(~r|^0x[A-Fa-f0-9]+$|, word) ->
+        "0x" <> hex_code = word
+        {val, ""} = Integer.parse(hex_code, 16)
+        {:integer, val}
+
+      Regex.match?(~r|^0b[01]+$|, word) ->
+        "0b" <> binary_form = word
+        {val, ""} = Integer.parse(binary_form, 2)
+        {:integer, val}
+
+      Regex.match?(~r|^0o[0-7]+$|, word) ->
+        "0o" <> octal_form = word
+        {val, ""} = Integer.parse(octal_form, 8)
+        {:integer, val}
+
+      Regex.match?(~r|^[0-9]+$|, word) ->
+        {val, ""} = Integer.parse(word)
+        {:integer, val}
+
+      Regex.match?(~r|^-[0-9]+$|, word) ->
+        {val, ""} = Integer.parse(word)
+        {:integer, val}
+
+      Regex.match?(~r|^-[0-9]+\.[0-9]+$|, word) ->
+        {val, ""} = Float.parse(word)
+        {:float, val}
+
+      Regex.match?(~r|^[0-9]+\.[0-9]+$|, word) ->
+        {val, ""} = Float.parse(word)
+        {:float, val}
+
+      Regex.match?(~r|^-[0-9]+\.[0-9]+e[\-0-9]+$|, word) ->
+        {val, ""} = Float.parse(word)
+        {:float, val}
+
+      Regex.match?(~r|^[0-9]+\.[0-9]+e[\-0-9]+$|, word) ->
+        {val, ""} = Float.parse(word)
+        {:float, val}
+
+      true -> word
+    end
+  end
+
+  defp lex_second_stage(lexed, lex_2 \\ [])
+  defp lex_second_stage([], lex_2), do: lex_2
+
+  # Transform minus (integer) into negative number
+  defp lex_second_stage([:minus, {:integer, num} | rest], lex_2),
+    do: lex_second_stage([{:integer, -num} | rest], lex_2)
+
+  # Transform minus (float) into negative number
+  defp lex_second_stage([:minus, {:float, num} | rest], lex_2),
+      do: lex_second_stage([{:float, -num} | rest], lex_2)
+
+  # Double negation is nothing - ignore it
+  defp lex_second_stage([:exclamation, :exclamation | rest], lex_2),
+       do: lex_second_stage(rest, lex_2)
+
+  defp lex_second_stage([:not, :not | rest], lex_2),
+       do: lex_second_stage(rest, lex_2)
+
+  defp lex_second_stage([:exclamation, :true | rest], lex_2),
+       do: lex_second_stage([:false | rest], lex_2)
+
+  defp lex_second_stage([:not, :true | rest], lex_2),
+       do: lex_second_stage([:false | rest], lex_2)
+
+  defp lex_second_stage([:exclamation, :false | rest], lex_2),
+       do: lex_second_stage([:true | rest], lex_2)
+
+  defp lex_second_stage([:not, :false | rest], lex_2),
+       do: lex_second_stage([:true | rest], lex_2)
+
+  # Transform != into :not_equals
+  defp lex_second_stage([:exclamation, :equals | rest], lex_2),
+       do: lex_second_stage([:not_equals | rest], lex_2)
+
+  # Transforms <= into :lt_or_equals
+  defp lex_second_stage([:left_arrow, :equals | rest], lex_2),
+       do: lex_second_stage([:lt_or_equals | rest], lex_2)
+
+  # Transforms << into :bit_shift_left
+  defp lex_second_stage([:left_arrow, :left_arrow | rest], lex_2),
+       do: lex_second_stage([:bit_shift_left | rest], lex_2)
+
+  # Transforms >= into :gt_or_equals
+  defp lex_second_stage([:right_arrow, :equals | rest], lex_2),
+       do: lex_second_stage([:gt_or_equals | rest], lex_2)
+
+  # Transforms >> into :bit_shift_right
+  defp lex_second_stage([:right_arrow, :right_arrow | rest], lex_2),
+       do: lex_second_stage([:bit_shift_right | rest], lex_2)
+
+  # Transforms == into :has_equality
+  defp lex_second_stage([:equals, :equals | rest], lex_2),
+       do: lex_second_stage([:has_equality | rest], lex_2)
+
+  # Passthrough for all unknown sequences
+  defp lex_second_stage([word | rest], lex_2),
+       do: lex_second_stage(rest, lex_2 ++ [word])
 end
--- test/expressions/lex_test.exs
+++ test/expressions/lex_test.exs
@@ -4,10 +4,93 @@ defmodule KaitaiToolkitTest.Expressions.LexTest do
   alias KaitaiToolkit.Ksy.Expression
 
   test "parses a basic sentence" do
-    assert ["if", "something", "=", "1"] = Expression.lex("if something = 1")
+    assert ["if", "something", :equals, {:integer, 1}] = Expression.lex("if something = 1")
   end
 
-  test "ignors numeric _" do
-    assert ["123", "+", "456.789"] = Expression.lex("1_23 + 45_6.7_8_9")
+  test "ignores numeric _" do
+    assert [{:integer, 123}, :plus, {:float, 456.789}] = Expression.lex("1_23 + 45_6.7_8_9")
+  end
+
+  test "handles scientific notation" do
+    assert [{:float, a}, :star, {:float, b}] = Expression.lex("1.23e-1 * 1.23e2")
+    assert_in_delta(a, 0.123, 0.001)
+    assert_in_delta(b, 123.0, 0.001)
+
+    assert [{:float, a}, :star, {:float, b}] = Expression.lex("1.23e-1 * -1.23e2")
+    assert_in_delta(a, 0.123, 0.001)
+    assert_in_delta(b, -123.0, 0.001)
+  end
+
+  test "handles negative numbers" do
+    assert [{:integer, -123}, :plus, {:float, -456.789}] = Expression.lex("-1_23 + -45_6.7_8_9")
+  end
+
+  test "handles booleans" do
+    assert ["abc", :not_equals, :true] = Expression.lex("abc != true")
+    assert ["abc", :has_equality, :false] = Expression.lex("abc == false")
+  end
+
+  test "handles bit manipulations" do
+    assert ["abc", :bit_shift_right, {:integer, 2}, :has_equality, {:integer, 128}] = Expression.lex("abc >> 2 == 128")
+    assert ["abc", :bit_shift_left, {:integer, 2}, :has_equality, {:integer, 128}] = Expression.lex("abc << 2 == 128")
+    assert ["a", :ampersand, {:integer, 1}, :has_equality, :true] = Expression.lex("a & 1 == true")
+    assert ["a", :pipe, {:integer, 1}, :has_equality, :true] = Expression.lex("a | 1 == true")
+    assert ["a", :caret, {:integer, 1}, :has_equality, :true] = Expression.lex("a ^ 1 == true")
+  end
+
+  test "handles logical operators" do
+    assert ["a", :and, :true] = Expression.lex("a and true")
+    assert ["a", :and, :false] = Expression.lex("a and not true")
+    assert ["a", :and, :not, "b"] = Expression.lex("a and not b")
+    assert ["a", :or, :not, "b"] = Expression.lex("a or not b")
+  end
+
+  test "handles ternary operators" do
+    assert ["a", :question_mark, "b", :colon, "c"] = Expression.lex("a ? b : c")
+  end
+
+  test "handles method calls" do
+    assert ["a", :dot, "method", :open_parens, {:integer, 123}, :close_parens] = Expression.lex("a.method(123)")
+  end
+
+  test "handles arrows" do
+    assert ["abc", :left_arrow, {:integer, 123}] = Expression.lex("abc < 123")
+    assert ["abc", :right_arrow, {:integer, 123}] = Expression.lex("abc > 123")
+    assert ["abc", :lt_or_equals, {:integer, 123}] = Expression.lex("abc <= 123")
+    assert ["abc", :gt_or_equals, {:integer, 123}] = Expression.lex("abc >= 123")
+  end
+
+  test "handles brackets" do
+    assert [:open_square_bracket, {:integer, 1}, :close_square_bracket] = Expression.lex("[1]")
+    assert [:open_square_bracket, {:integer, 1}, :comma, "abc", :comma, {:integer, 2}, :close_square_bracket] = Expression.lex("[1, abc, 2]")
+  end
+
+  test "handles single quotes" do
+    assert [:single_quote, {:integer, 1}, :single_quote] = Expression.lex("'1'")
+    assert [:single_quote, "abc", :single_quote] = Expression.lex("'abc'")
+  end
+
+  test "handles double quotes" do
+    assert [:double_quote, {:integer, 1}, :double_quote] = Expression.lex(~s|"1"|)
+    assert [:double_quote, "abc", :double_quote] = Expression.lex(~s|"abc"|)
+  end
+
+  test "handles backslashes" do
+    assert [:double_quote, :backslash, "a", :backslash, "b", :double_quote] = Expression.lex(~s|"\\a\\b"|)
+  end
+
+  test "handles private variables" do
+    assert [:io] = Expression.lex(~s|_io|)
+    assert [:root] = Expression.lex(~s|_root|)
+    assert [:parent] = Expression.lex(~s|_parent|)
+  end
+
+  test "handles negation of literals" do
+    assert [:true] = Expression.lex("!false")
+    assert [:false] = Expression.lex("!true")
+    assert [:false] = Expression.lex("!!false")
+    assert [:true] = Expression.lex("!!true")
+    assert [:true] = Expression.lex("!!!false")
+    assert [:false] = Expression.lex("!!!true")
   end
 end
